3fcad9c8fca029d6ea070049dadf69f20f74f14f
Mohammed <alkatebmohammed383@gmail.com>
2024-05-01 19:11:07 +0200

    Configure socket.io and http server into server.js

--- backend/server.js
+++ backend/server.js
@@ -1,24 +1,38 @@
 import express from "express";
+import http from "http";
 import helmet from "helmet";
 import dotenv from "dotenv";
 import cookieParser from "cookie-parser";
+import cors from "cors";
 import { mountDB, unmountDB } from "./utils/dbConnection.js";
-import usersRouter from './routers/users.js'; 
+import { Server } from "socket.io";
+import usersRouter from './routers/users.js';
+import authRouter from "./routers/authRouter.js";
 dotenv.config();
 
+const app = express();
+
 const EXPRESS_PORT = 3000;
-const server = express();
+const server = http.createServer(express());
+const io = new Server(server, {
+    cors: {
+        origin: "http://localhost:3000",
+        methods: ["GET", "POST", "PUT", "DELETE"],
+    },
+});
 mountDB();
 
 // Assign middlewares
-server.use(express.json());
-server.use(express.urlencoded({ extended: true }));
-server.use(cookieParser());
-server.use(helmet());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(cookieParser());
+app.use(helmet());
+app.use(cors());
 
 // Here should routers be used:
 // ex: server.use("/debates", debatesRouter);
-server.use("/api/v1/users", usersRouter);
+app.use("/api/v1/users", usersRouter);
+app.use('/api/v1/auth', authRouter);
 
 server.listen(EXPRESS_PORT, (err) => {
     err
