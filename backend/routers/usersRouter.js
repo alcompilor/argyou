69999e9173ab4e656fddbd82658be85dc0bfaacd
Ahmed Abbasi <contact@ahmed.dev>
2024-05-03 14:10:31 +0200

    Add authentication and authorization middlewares to endpoints

--- backend/routers/usersRouter.js
+++ backend/routers/usersRouter.js
@@ -1,12 +1,14 @@
 import express from 'express';
 import { createUser, getUser, updateUser, deleteUser } from '../controllers/usersController.js';
+import isAuth from "../middlewares/isAuth.js";
+import hasPermission from "../middlewares/hasPermission.js";
 
 const usersRouter = express.Router();
 
 usersRouter.route("/:username")
-    .get(getUser)
-    .patch(updateUser)
-    .delete(deleteUser);
+    .get(isAuth, hasPermission, getUser)
+    .patch(isAuth, hasPermission, updateUser)
+    .delete(isAuth, hasPermission, deleteUser);
 
 usersRouter.post('/', createUser);
 
