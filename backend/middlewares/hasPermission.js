d7d51c2ac173eb84ec3632905352701c109e0186
shuaybw <shuaybwar@gmail.com>
2024-05-04 18:26:33 +0200

    Updated the permission checks to cover all database collections

--- backend/middlewares/hasPermission.js
+++ backend/middlewares/hasPermission.js
@@ -1,22 +1,30 @@
 import mongoose from 'mongoose';
 
 const hasPermission = (collectionName, documentField) => async (req, res, next) => {
-  // Use decoded token from isAuth middleware
   const decoded = req.decodedToken;
-    
-    try {
-      const decoded = jwt.verify(token, process.env.SECRET_KEY);
-      req.user = decoded;
+
+  try {
+    const collection = mongoose.connection.collection(collectionName);
+    const query = {};
+    query[documentField] = req.params[documentField];
+    const document = await collection.findOne(query);
 
 
+    if (!document) {
+        return res.status(404).send('Resource attempted to access was not found.');
+    }
 
-      // Check if the username needs to match the URL parameter
-      if (matchUsernameParam && req.params[matchUsernameParam] !== decoded.username) {
-        return res.status(403).send('Access denied. Username mismatch.');
-      }
 
-      
-  };
+    if (!decoded.isAdmin && document.owner !== decoded.username) {
+        return res.status(403).send('Access denied. Insufficient permissions.');
+    }
+
+
+    next();
+} catch (error) {
+    res.status(500).send(`Internal server error: ${error.message}`); 
+}
 };
+
 
 export default hasPermission;
