de52349221bf33d3472b79a4dd80befe3ec3d0b6
Ahmed Abbasi <contact@ahmed.dev>
2024-05-04 19:09:47 +0200

    Refactor `usersModel` - Added `owner` field with custom validator - Added `avatar` field with custom validator - Changed name of model from `User` to `users`
    Co-authored-by: Mohamed Nour Humeidi <mohamednourhumeidi@gmail.com>

--- backend/models/usersModel.js
+++ backend/models/usersModel.js
@@ -5,6 +5,29 @@ const emailRegex = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+")
 const passwordRegex = /^(?=.*[A-Z])(?=.*[a-z])(?=.*\d)(?=.*[!@#$%^&*()_+}{":;'?/><.,])(?!.*\s).{10,}$/;
 const usernameRegex = /^[a-zA-Z0-9]+([_ -]?[a-zA-Z0-9])*$/;
 
+const validateAvatar = (avatar, sizeInMB) => {
+    if (!avatar || avatar.length === 0) {
+        return false;
+    }
+
+    const maxSizeInBytes = sizeInMB * 1024 * 1024; // Max avatar size
+    if (avatar.length > maxSizeInBytes) {
+        return false;
+    }
+
+    // Check if the buffer starts with the signature of a PNG, JPEG
+    const supportedImageSignatures = [
+        Buffer.from([0x89, 0x50, 0x4E, 0x47]), // PNG sig
+        Buffer.from([0xFF, 0xD8, 0xFF])      // JPEG sig
+    ];
+    const signature = avatar.slice(0, 4);
+    if (!supportedImageSignatures.some(sig => signature.equals(sig))) {
+        return false;
+    }
+
+    return true;
+};
+
 const userSchema = new mongoose.Schema({
     fullName: {
         type: String,
@@ -40,10 +63,33 @@ const userSchema = new mongoose.Schema({
         type: Boolean,
         required: true
     },
-    debates: [String],
+    avatar: {
+        type: Buffer,
+        validate: {
+            validator: avatar => validateAvatar(avatar, 2),
+            message: "Avatar must be a .PNG or .JPEG image, and less than 2MB in size"
+        }
+    },
+    debates: [{
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "debates"
+    }],
     inDebate: {
         type: Boolean,
         default: false
+    },
+    owner: {
+        type: String,
+        default: function () {
+            return this.username;
+        },
+        immutable: true,
+        validate: {
+            validator: function(owner) {
+                return owner === this.username;
+            },
+            message: 'Owner must match username'
+        },
     },
     isAdmin: {
         type: Boolean,
@@ -63,11 +109,13 @@ userSchema.pre('save', async function(next) {
 userSchema.pre('findOneAndUpdate', async function (next) {
     const data = this.getUpdate();
     
-    const salt = await bcrypt.genSalt(10);
-    const hashedPassword = await bcrypt.hash(data.password, salt);
-    data.password = hashedPassword;
+    if (data.password) {
+        const salt = await bcrypt.genSalt(10);
+        const hashedPassword = await bcrypt.hash(data.password, salt);
+        data.password = hashedPassword;
+    }
     next();
 });
 
-const User = mongoose.model('User', userSchema);
+const User = mongoose.model('users', userSchema);
 export default User;
