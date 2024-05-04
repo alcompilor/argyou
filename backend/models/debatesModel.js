54ac95d4db926a22b5b8a8d003baa4a6cbebeb87
Mohammed <alkatebmohammed383@gmail.com>
2024-05-04 13:30:56 +0200

    Add parameters for hasPermission middleware in debatesRouter endpoints and update debatesController to use request's _id instead of id.

--- backend/models/debatesModel.js
+++ backend/models/debatesModel.js
@@ -4,120 +4,130 @@ const Schema = mongoose.Schema;
 
 // 00:00 time format
 function validateTime(startTime) {
-    const timeRegex = /^(0[0-9]|1[0-9]|2[0-3]):[0-5][0-9]$/;
-    return timeRegex.test(startTime);
+  const timeRegex = /^(0[0-9]|1[0-9]|2[0-3]):[0-5][0-9]$/;
+  return timeRegex.test(startTime);
 }
 
 // three questions required
 function validateQuestions(questions) {
-    return questions.length === 3;
+  return questions.length === 3;
 }
 
 // thumbnail of type buffer
 function validateThumbnail(thumbnail) {
-    if (!Buffer.isBuffer(thumbnail)) {
-        throw new Error('Thumbnail must be stored as a binary buffer');
-    }
+  if (!Buffer.isBuffer(thumbnail)) {
+    throw new Error("Thumbnail must be stored as a binary buffer");
+  }
 }
 
 const debatesSchema = new Schema({
-    title: {
-        type: String,
-        required: [true, 'Debate title is required'],
-        minLength: [20, 'Too short title'],
-        maxLength: 100,
-    },
-    creatorUsername: {
-        type: String,
-        required: [true, "Debate creator's id is required"],
-        ref: 'users'
-    },
-    opponentUsername: {
-        type: String, 
-        ref: 'users'
-    },
-    startTime: {
-        type: String,
-        required: [true, 'Predefined start time is needed'],
-        validate: [validateTime, 'Invalid start time format. Please use "00:00" format.'],
-    },
-    endTime: {
-        type: String,
-        validate: [validateTime, 'Invalid end time format. Please use "00:00" format.'],
-    },
-    messages: {
-        type: [{
-            content: {
-                type: String,
-                required: true,
-                minLength: 10,
-                maxLength: 500
-            },
-            username: {
-                type: String,
-                required: true,
-                ref: 'users',
-            },
-            publishDate: {
-                type: Date,
-                default: Date.now
-            }
-        }]
-    },
-    thumbnail: {
-        type: Buffer,
-        required: [true, 'A thumbnail is required'],
-        validate: [validateThumbnail, 'A thumbnail required']
-    },
-    questions: {
-        type: Array,
-        required: true,
-        validate: [validateQuestions, 'There should be three questions']
-    },
-    status: {
-        type: String,
-        enum: {
-            values: ['Waiting Participant', 'Ongoing', 'Shookhands', 'Ended'],
-            message: 'Debate state is not supported'
+  title: {
+    type: String,
+    required: [true, "Debate title is required"],
+    minLength: [20, "Too short title"],
+    maxLength: 100,
+  },
+  creatorUsername: {
+    type: String,
+    required: [true, "Debate creator's id is required"],
+    ref: "users",
+  },
+  opponentUsername: {
+    type: String,
+    ref: "users",
+  },
+  startTime: {
+    type: String,
+    required: [true, "Predefined start time is needed"],
+    validate: [
+      validateTime,
+      'Invalid start time format. Please use "00:00" format.',
+    ],
+  },
+  endTime: {
+    type: String,
+    validate: [
+      validateTime,
+      'Invalid end time format. Please use "00:00" format.',
+    ],
+  },
+  messages: {
+    type: [
+      {
+        content: {
+          type: String,
+          required: true,
+          minLength: 10,
+          maxLength: 500,
         },
-        default: "Waiting Participant"
+        username: {
+          type: String,
+          required: true,
+          ref: "users",
+        },
+        publishDate: {
+          type: Date,
+          default: Date.now,
+        },
+      },
+    ],
+  },
+  thumbnail: {
+    type: Buffer,
+    required: [true, "A thumbnail is required"],
+    validate: [validateThumbnail, "A thumbnail required"],
+  },
+  questions: {
+    type: Array,
+    required: true,
+    validate: [validateQuestions, "There should be three questions"],
+  },
+  status: {
+    type: String,
+    enum: {
+      values: ["Waiting Participant", "Ongoing", "Shookhands", "Ended"],
+      message: "Debate state is not supported",
     },
-    comments: {
-        type: [{
-            username: {
-                type: String,
-                required: true,
-                ref: 'users',
-            },
-            content: {
-                type: String,
-                required: true,
-                minLength: 5,
-                maxLength: 500
-            },
-            publishDate: {
-                type: Date,
-                default: Date.now
-            }
-        }]
-    }
+    default: "Waiting Participant",
+  },
+  comments: {
+    type: [
+      {
+        username: {
+          type: String,
+          required: true,
+          ref: "users",
+        },
+        content: {
+          type: String,
+          required: true,
+          minLength: 5,
+          maxLength: 500,
+        },
+        publishDate: {
+          type: Date,
+          default: Date.now,
+        },
+      },
+    ],
+  },
 });
 
 // prevent user from entering own time
-debatesSchema.path('messages').set(function(messages) {
-    messages.forEach(message => {
-        message.publishDate = this.isNew ? message.publishDate : Date.now();
-    });
-    return messages;
+debatesSchema.path("messages").set(function (messages) {
+  messages.forEach((message) => {
+    message.publishDate = this.isNew ? message.publishDate : Date.now();
+  });
+  return messages;
 });
 
-debatesSchema.path('comments').set(function(comments) {
-    comments.forEach(comment => {
-        comment.publishDate = this.isNew ? comment.publishDate : Date.now();
-    });
-    return comments;
+debatesSchema.path("comments").set(function (comments) {
+  comments.forEach((comment) => {
+    comment.publishDate = this.isNew ? comment.publishDate : Date.now();
+  });
+  return comments;
 });
 
-const Debate = mongoose.model('debates', debatesSchema);
+const Debate = mongoose.model("debates", debatesSchema);
 
 export default Debate;
--- backend/routers/debatesRouter.js
+++ backend/routers/debatesRouter.js
@@ -1,27 +1,27 @@
 import express from "express";
-import { 
-    getAllDebates, 
-    getDebate, 
-    createDebate, 
-    updateDebate, 
-    deleteDebate, 
-    addComment
+import {
+  getAllDebates,
+  getDebate,
+  createDebate,
+  updateDebate,
+  deleteDebate,
+  addComment,
 } from "../controllers/debatesController.js";
 import hasPermission from "../middlewares/hasPermission.js";
 import isAuth from "../middlewares/isAuth.js";
 
 const debatesRouter = express.Router();
+const DEBATES_COLLECTION = "debates";
+const FIELD_NAME = "_id";
 
-debatesRouter.route('/')
-    .get(isAuth, getAllDebates)
-    .post(isAuth, createDebate);
+debatesRouter.route("/").get(isAuth, getAllDebates).post(isAuth, createDebate);
 
-debatesRouter.route('/:id')
-    .get(isAuth, hasPermission, getDebate)
-    .delete(isAuth, hasPermission, deleteDebate)
-    .patch(isAuth, hasPermission, updateDebate);
+debatesRouter
+  .route("/:_id")
+  .get(isAuth, hasPermission(DEBATES_COLLECTION, FIELD_NAME), getDebate)
+  .delete(isAuth, hasPermission(DEBATES_COLLECTION, FIELD_NAME), deleteDebate)
+  .patch(isAuth, hasPermission(DEBATES_COLLECTION, FIELD_NAME), updateDebate);
 
-debatesRouter.route('/:id/comments')
-    .patch(isAuth, addComment);
+debatesRouter.route("/:_id/comments").patch(isAuth, addComment);
 
 export default debatesRouter;
