4006431077a406fc0c3c1a244b9dbe2618170ff0
shuaybw <shuaybwar@gmail.com>
2024-05-05 22:18:23 +0200

    Implemented a new logout endpoint

---
+++ backend/controllers/logout.js
@@ -0,0 +1,15 @@
+const logout = (req, res) => {
+    try {
+        res.clearCookie('access_token');
+
+        res.status(200).json({ message: 'Successfully logged out 👋 🍀' });
+    } catch (error) {
+    
+        console.error("Logout Error:", error);
+
+       
+        res.status(500).json({ message: 'Failed to log out. Please try again later.' });
+    }
+};
+
+export default logout;  
