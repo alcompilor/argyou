1984e4a9beba36a5446f72350dd822256848efc5
shuaybw <shuaybwar@gmail.com>
2024-05-06 16:44:14 +0200

    removed console.error

--- backend/controllers/logout.js
+++ backend/controllers/logout.js
@@ -8,9 +8,6 @@ const logout = (req, res) => {
 
         res.status(200).json({ message: 'Successfully logged out'});
     } catch (error) {
-    
-        console.error("Logout Error:", error);
-
        
         res.status(500).json({ message: 'Failed to log out. Please try again later.' });
     }
