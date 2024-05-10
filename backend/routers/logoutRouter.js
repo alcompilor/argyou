4e96e81bf5bdd440d3bfc9b7c56c6ad4b81b0652
Ahmed Abbasi <contact@ahmed.dev>
2024-05-10 17:40:39 +0200

    Refactor module names for enhanced maintainability
    Co-authored-by: Mohammed Alkateb <alkatebmohammed383@gmail.com>
    Co-authored-by: Nour Humeidi <mohamednourhumeidi@gmail.com>
    Co-authored-by: Shuayb Warsame <shuaybwar@gmail.com>
    Co-authored-by: Joseph Hammami <josephhammamii@gmail.com>

--- backend/routers/logoutRouter.js
+++ backend/routers/logoutRouter.js
@@ -1,10 +1,8 @@
 import express from 'express';
-import logout from '../controllers/logout.js';
+import logout from '../controllers/logoutController.js';
 
 const logoutRouter = express.Router();
 
-
 logoutRouter.get('/', logout);
-
 
 export default logoutRouter;
