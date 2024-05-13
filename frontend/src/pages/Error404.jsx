ba7d56f2fb7b8c6f3649b4b4424e7fe17f12b0c6
shuaybw <shuaybwar@gmail.com>
2024-05-13 22:31:31 +0200

    removed Error404 component

--- frontend/src/pages/Error404.jsx
+++ frontend/src/pages/Error404.jsx
@@ -1,9 +1,15 @@
-import { ErrorContent } from '@/components/ErrorContent'; 
+import notFoundImage from '../assets/imgs/404.jpg'; 
+import { Navbar } from "../components/clientUI/Navbar.jsx";
+
+
 
 export const Error404 = () => {
     return (
-    
-    <ErrorContent />
-
+        <>
+            <Navbar />
+            <div className="flex items-center justify-center h-screen" style={{ marginTop: '-50px'}}>
+                <img src={notFoundImage} alt="404 Not Found" className="mx-auto max-w-md h-auto" />
+            </div>
+        </>
     );
 };
