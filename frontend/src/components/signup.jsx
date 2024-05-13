eb07d7673687576a11570c3bf4bb0388be53faf6
Nour <mohamednourhumeidi@gmail.com>
2024-05-13 10:36:53 +0200

    use flowbite react for user input fields

--- frontend/src/components/signup.jsx
+++ frontend/src/components/signup.jsx
@@ -1,5 +1,6 @@
 import React, { useState } from 'react';
 import './signup.css';
+import { Button, Label, TextInput } from "flowbite-react";
 
 const SignupPage = () => {
     const [action, setAction] = useState("Sign Up");
@@ -24,33 +25,51 @@ const SignupPage = () => {
                 <div className="text">{action}</div>
                 <div className='underline'></div>
             </div>
-            <form className="inputs" onSubmit={handleSubmit}>
-                <div className="input">
-                    <input
+            <form className="flex flex-col gap-4" onSubmit={handleSubmit}>
+                <div className="max-w-md">
+                    <div className="mb-2 block">
+                        <Label htmlFor="username" value="Username" />
+                    </div>
+                    <TextInput
+                        id="username"
                         type="text"
-                        placeholder='Username'
+                        placeholder="Bonnie Green"
+                        addon="@"
                         value={username}
                         onChange={(e) => setUsername(e.target.value)}
+                        required
                     />
                 </div>
-                <div className="input">
-                    <input
+                <div className="max-w-md">
+                    <div className="mb-2 block">
+                        <Label htmlFor="email" value="Email" />
+                    </div>
+                    <TextInput
+                        id="email"
                         type="email"
-                        placeholder='Email'
+                        placeholder="name@flowbite.com"
                         value={email}
                         onChange={(e) => setEmail(e.target.value)}
+                        required
                     />
                 </div>
-                <div className="input">
-                    <input
+                <div className="max-w-md">
+                    <div className="mb-2 block">
+                        <Label htmlFor="password" value="Password" />
+                    </div>
+                    <TextInput
+                        id="password"
                         type="password"
-                        placeholder='Password'
+                        placeholder="Password"
                         value={password}
                         onChange={(e) => setPassword(e.target.value)}
+                        required
                     />
                 </div>
                 <div className="submit-container">
-                    <button type='submit' className='submit'>Sign Up</button>
+                    <Button type="submit">
+                        Sign Up
+                    </Button>
                 </div>
             </form>
         </div>
