a795dcbcc7a52d8dd86fb73c5e6a218c44d9b297
shuaybw <shuaybwar@gmail.com>
2024-05-14 12:54:05 +0200

    Included Error404.jsx  as child dependency

--- frontend/src/pages/Error404.jsx
+++ frontend/src/pages/Error404.jsx
@@ -1,12 +1,9 @@
 import notFoundImage from '../assets/imgs/404.jpg'; 
-import { Navbar } from "../components/clientUI/Navbar.jsx";
-
 
 
 export const Error404 = () => {
     return (
         <>
-            <Navbar />
             <div className="flex items-center justify-center h-screen" style={{ marginTop: '-50px'}}>
                 <img src={notFoundImage} alt="404 Not Found" className="mx-auto max-w-md h-auto" />
             </div>
