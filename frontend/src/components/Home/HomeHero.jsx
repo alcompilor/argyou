42ec986ea76b4a2640b3cfc042c9aa003b376063
Ahmed Abbasi <alcompilor@gmail.com>
2024-05-16 23:23:44 +0200

    Enhance `HomeHero` styling for large screens
    Co-authored-by: Shuayb Warsame <shuaybwar@gmail.com>

--- frontend/src/components/Home/HomeHero.jsx
+++ frontend/src/components/Home/HomeHero.jsx
@@ -4,14 +4,14 @@ import debateIllustration from "@/assets/imgs/debate_illustration.svg";
 export const HomeHero = () => {
     return (
         <div className="relative flex flex-col-reverse py-16 lg:pt-0 lg:flex-col lg:pb-0 bg-gray-50">
-            <div className="inset-y-0 top-0 right-0 z-0 w-full max-w-xl px-4 mx-auto md:px-0 lg:pr-0 lg:mb-0 lg:mx-0 lg:w-7/12 lg:max-w-full lg:absolute xl:px-0">
+            <div className="inset-y-0 top-0 right-0 z-0 w-full max-w-xl px-4 mx-auto md:px-0 lg:pr-0 lg:mb-0 lg:mx-0 lg:w-7/12 lg:max-w-4xl lg:absolute xl:px-0">
                 <img
-                    className="object-cover w-full h-56 rounded shadow-lg lg:rounded-none lg:shadow-none md:h-96 lg:h-full"
+                    className="object-cover w-full h-56 rounded lg:rounded-none lg:shadow-none md:h-96 lg:h-full"
                     src={debateIllustration}
                     alt="Debate Illustration"
                 />
             </div>
-            <div className="relative flex flex-col items-start w-full max-w-xl px-4 mx-auto md:px-0 lg:px-8 lg:max-w-screen-xl">
+            <div className="relative flex flex-col items-start w-full max-w-xl px-4 mx-auto md:px-0 lg:px-8 lg:max-w-screen-2xl">
                 <div className="mb-16 lg:my-40 lg:max-w-lg lg:pr-5">
                     <p className="inline-block px-3 py-px mb-4 text-xs font-semibold tracking-wider text-white uppercase rounded-full bg-rose-500">
                         Beta
