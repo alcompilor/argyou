7d69bdc3256b890e2a086e53146c6d199c0b7ac8
Ahmed Abbasi <alcompilor@gmail.com>
2024-05-18 00:18:08 +0200

    Refactor `Navbar.jsx` and `Footer.jsx` to enhance their stylings - Removed `MenuNavbar.jsx` as `Navbar.jsx` is now handling mobile menu as well
    Co-authored-by: Mohammed Alkateb <alkatebmohammed383@gmail.com>
    Co-authored-by: Nour Humeidi <mohamednourhumeidi@gmail.com>
    Co-authored-by: Shuayb Warsame <shuaybwar@gmail.com>
    Co-authored-by: Joseph Hammami <josephhammamii@gmail.com>

--- frontend/src/components/clientUI/Navbar.jsx
+++ frontend/src/components/clientUI/Navbar.jsx
@@ -1,77 +1,62 @@
 import { NavLink } from "react-router-dom";
 import { Logo } from "./Logo.jsx";
-import { IconBell, IconSearch } from "@tabler/icons-react";
+import { IconBell } from "@tabler/icons-react";
+import { Navbar as NavbarFlowBite } from "flowbite-react";
 
 export const Navbar = ({ isLoggedIn }) => {
-  const styleLink =
-    "transition-colors duration-300 ease-in-out hover:bg-gray-700 rounded-lg p-2";
-  return (
-    <nav className="bg-rose-500 min-h-[9vh] w-full">
-      <div className="container mx-auto md:flex justify-between items-center px-4">
-        <ul className="md:flex text-white font-bold mt-4 md:mt-0 text-sm">
-          <Logo
-            width={150}
-            height={50}
-            className={"py-4 px-5"}
-            logoType={"white"}
-          />
-          <li className="py-6 px-4">
-            <NavLink to="/" className={styleLink}>
-              Home
+    const styleLink =
+        "transition-colors duration-300 ease-in-out hover:bg-gray-800 rounded-lg p-2 text-white font-bold";
+    return (
+        <NavbarFlowBite className="bg-rose-500 md:py-4 py-5 px-6">
+            <NavLink to="/">
+                <Logo className="w-32" logoType="white" />
             </NavLink>
-          </li>
-          <li className="py-6 px-4">
-            <NavLink to="/about-us" className={styleLink}>
-              About Us
-            </NavLink>
-          </li>
-        </ul>
-        <div className="relative flex items-center">
-          <input
-            type="text"
-            placeholder="Search..."
-            className="rounded-lg py-1 pr-12 focus:outline-gray-800 text-gray-800 w-full"
-          />
-          <IconSearch className="absolute right-3 text-gray-500 h-6 w-6" />
-        </div>
-        <ul className="md:flex gap-2 text-white font-bold mt-4 md:mt-0 text-sm">
-          {isLoggedIn ? (
-            <>
-              <li className="py-3.5 px-4">
-                <NavLink to="/debate-space" className={styleLink}>
-                  Debate Space
+            <div className="flex md:order-2 gap-2 items-center">
+                {isLoggedIn ? (
+                    <NavLink
+                        to="/debate-space"
+                        className="text-rose-600 hover:bg-rose-50 bg-rose-100  focus:ring-4 focus:outline-none focus:ring-rose-300 font-semibold rounded-lg text-sm px-4 py-2 text-center"
+                    >
+                        Debate Space
+                    </NavLink>
+                ) : (
+                    <NavLink
+                        to="/login"
+                        className="text-rose-600 hover:bg-rose-50 bg-rose-100  focus:ring-4 focus:outline-none focus:ring-rose-300 font-semibold rounded-lg text-sm px-4 py-2 text-center"
+                    >
+                        Sign In
+                    </NavLink>
+                )}
+                <NavbarFlowBite.Toggle className="text-white hover:bg-gray-800" />
+            </div>
+            <NavbarFlowBite.Collapse>
+                <NavLink to="/" className={styleLink}>
+                    Home
                 </NavLink>
-              </li>
-              <li className="py-3.5 px-4">
-                <NavLink to="/profile" className={styleLink}>
-                  Profile
+                <NavLink to="/about-us" className={styleLink}>
+                    About Us
                 </NavLink>
-              </li>
-              <li className="relative py-3.5 px-4">
-                <IconBell className="absolut text-white-500 fill-current hover:fill-rose-500" />
-              </li>
-              <li className="py-3.5 px-4">
-                <NavLink to="/logout" className={styleLink}>
-                  Logout
-                </NavLink>
-              </li>
-            </>
-          ) : (
-            <>
-              <li className="py-3.5 px-4">
-                <NavLink to="/login" className={styleLink}>
-                  Login
-                </NavLink>
-              </li>
-              <li className="py-3.5 px-4">
-                <NavLink to="/signup" className={styleLink}>
-                  Signup
-                </NavLink>
-              </li>
-            </>
-          )}
-        </ul>
-      </div>
-    </nav>
-  );
+                {isLoggedIn ? (
+                    <>
+                        <NavLink
+                            to="/profile"
+                            className={`${styleLink} flex items-center gap-2`}
+                        >
+                            <li>
+                                <IconBell className="absolut text-white-500 fill-current hover:fill-rose-500" />
+                            </li>
+                            Profile
+                        </NavLink>
+                        <NavLink to="/logout" className={styleLink}>
+                            Logout
+                        </NavLink>
+                    </>
+                ) : (
+                    <NavLink to="/signup" className={styleLink}>
+                        Create an Account
+                    </NavLink>
+                )}
+            </NavbarFlowBite.Collapse>
+        </NavbarFlowBite>
+    );
 };
