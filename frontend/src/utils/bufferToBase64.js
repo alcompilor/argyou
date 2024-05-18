d9da8d9fe6fe8b735829c469e069ed301d7a76dc
Ahmed Abbasi <contact@ahmed.dev>
2024-05-19 01:01:01 +0200

    Enhance algorithm to be able to process huge images in chunks Prevents the error `Maximum call stack size exceeded`

--- frontend/src/utils/bufferToBase64.js
+++ frontend/src/utils/bufferToBase64.js
@@ -1,3 +1,14 @@
 export const bufferToBase64 = (buffer) => {
-    return btoa(String.fromCharCode(...new Uint8Array(buffer)));
+    const uint8Array = new Uint8Array(buffer);
+    let binaryString = '';
+    const chunkSize = 0x8000;
+
+    for (let i = 0; i < uint8Array.length; i += chunkSize) {
+        binaryString += String.fromCharCode.apply(
+            null,
+            uint8Array.subarray(i, i + chunkSize)
+        );
+    }
+
+    return btoa(binaryString);
 };
