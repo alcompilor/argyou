97aeab7d89f6eafaad5c204f19d344ecca1f1231
Mohammed <alkatebmohammed383@gmail.com>
2024-05-30 18:45:11 +0200

    heatBar visualization

---
+++ frontend/src/components/HeatBar.jsx
@@ -0,0 +1,35 @@
+import React, { useEffect, useState } from "react";
+
+export const HeatBar = ({ heatScore }) => {
+  const [heatScoreInPercent, setHeatScoreInPercent] = useState(0);
+  const [barColor, setBarColor] = useState("none");
+
+  useEffect(() => {
+    if (heatScore < 1 && heatScore > 0) {
+      setHeatScoreInPercent(heatScore * 100);
+    }
+
+    if (heatScoreInPercent < 33.3) {
+      setBarColor("bg-green-400 dark:bg-green-500");
+    } else if (heatScoreInPercent < 66.6) {
+      setBarColor("bg-yellow-300 dark:bg-yellow-500");
+    } else if (heatScoreInPercent <= 100.0) {
+      setBarColor("bg-red-500 dark:bg-red-500");
+    } else {
+      return "Wrong heat value";
+    }
+  });
+
+  return (
+    <>
+      <div className="py-12 px-4 flex justify-center items-center">
+        <div className="w-3.5 bg-gray-200 rounded h-60 mb-4 dark:bg-gray-700 relative">
+          <div
+            className={`${barColor} w-3.5 rounded absolute bottom-0 duration-300`}
+            style={{ height: `${heatScoreInPercent}%` }}
+          ></div>
+        </div>
+      </div>
+    </>
+  );
+};
