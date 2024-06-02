40fb97afcc6f43fb30d2740c74fbeaa337cd60d8
Mohammed <alkatebmohammed383@gmail.com>
2024-06-02 12:24:17 +0200

    Add setInterval to prevent potential infinite loops

--- frontend/src/components/HeatBar.jsx
+++ frontend/src/components/HeatBar.jsx
@@ -2,22 +2,22 @@ import React, { useEffect, useState } from "react";
 
 export const HeatBar = ({ heatScore }) => {
   const [heatScoreInPercent, setHeatScoreInPercent] = useState(0);
-  const [barColor, setBarColor] = useState("none");
+  const [barColor, setBarColor] = useState("bg-gray-200 dark:bg-gray-400");
 
   useEffect(() => {
-    if (heatScore < 1 && heatScore > 0) {
+    const interval = setInterval(() => {
       setHeatScoreInPercent(heatScore * 100);
-    }
-
-    if (heatScoreInPercent < 33.3) {
-      setBarColor("bg-green-400 dark:bg-green-500");
-    } else if (heatScoreInPercent < 66.6) {
-      setBarColor("bg-yellow-300 dark:bg-yellow-500");
-    } else if (heatScoreInPercent <= 100.0) {
-      setBarColor("bg-red-500 dark:bg-red-500");
-    } else {
-      return "Wrong heat value";
-    }
+      
+      if (heatScoreInPercent < 33.3) {
+        setBarColor("bg-green-400 dark:bg-green-500");
+      } else if (heatScoreInPercent < 66.6) {
+        setBarColor("bg-yellow-300 dark:bg-yellow-500");
+      } else if (heatScoreInPercent <= 100.0) {
+        setBarColor("bg-red-500 dark:bg-red-500");
+      }
+    }, 2000);
+    
+    return () => clearInterval(interval);
   });
 
   return (
@@ -26,7 +26,7 @@ export const HeatBar = ({ heatScore }) => {
         <div className="w-3.5 bg-gray-200 rounded h-60 mb-4 dark:bg-gray-700 relative">
           <div
             className={`${barColor} w-3.5 rounded absolute bottom-0 duration-300`}
-            style={{ height: `${heatScoreInPercent}%` }}
+            style={{height: `${heatScoreInPercent}%`}}
           ></div>
         </div>
       </div>
