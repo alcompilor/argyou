9eecea7e6fdbf835bd9e17c046415bae8e0e66b3
Nour <mohamednourhumeidi@gmail.com>
2024-06-06 13:58:33 +0200

    set router for debate-space

--- frontend/src/Router.jsx
+++ frontend/src/Router.jsx
@@ -2,7 +2,11 @@ import { createBrowserRouter } from "react-router-dom";
 import { About } from "./pages/About";
 import { Layout } from "./Layout";
 import { Error404 } from "./pages/Error404";
-import { Signup } from "./components/signup";
+import { Home } from "./pages/Home";
+import { SignIn } from "./pages/SignIn";
+import { Profile } from "./pages/Profile";
+import {SignUp} from "./pages/SignUp";
+import { Admin } from "./pages/Admin";
 import { DebateSpace } from "./pages/DebateSpace";
 
 // You can add new routes in the children array to render pages.
@@ -19,25 +23,40 @@ export const router = createBrowserRouter([
     errorElement: <Error404 />,
     children: [
       {
+        index: true,
+        element: <Home />,
+      },
+      {
         path: "/about-us",
         element: <About />,
         errorElement: <Error404 />,
-        children: [
-          {
-            index: true,
-            element: <Signup />
-          },
-          {
-            path: "/signup",
-            element: <Signup />
-          }
-        ]
+        children: [],
+      },
+      {
+        path: "/login",
+        element: <SignIn />,
+        errorElement: <Error404 />,
+      },
+      {
+        path: "/profile",
+        element: <Profile />,
+        errorElement: <Error404 />,
+      },
+      {
+        path: "/signup",
+        element: <SignUp />,
+        errorElement: <Error404 />,
+      },
+      {
+        path: "/admin-panel",
+        element: <Admin />,
+        errorElement: <Error404 />,
       },
       {
         path: "/debate-space",
         element: <DebateSpace />,
-        errorElement: <Error404 />
+        errorElement: <Error404 />,
       }
-    ]
-  }
+    ],
+  },
 ]);
